b151bd6f6e87cf81614ee5b4b3da88ccc0708edc
Bruno Montebello <brunormontebello@hotmail.com>
2021-01-19 17:37:35 +0000

    Capture the elements needed

---
+++ app.js
@@ -0,0 +1,8 @@
+// set initial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector('#value');
+const buttons = document.querySelectorAll('.btn'); //will give is a NODE LIST, which is array-like. So we can call some methods on it
+
+console.log(buttons);
