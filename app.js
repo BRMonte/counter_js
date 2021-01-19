760f5cd0a4b532c8a5839e25a8f2fab69df4fa90
Bruno Montebello <brunormontebello@hotmail.com>
2021-01-19 17:58:28 +0000

    Create increase, decrease and reset logic

--- app.js
+++ app.js
@@ -5,4 +5,18 @@ let count = 0;
 const value = document.querySelector('#value');
 const buttons = document.querySelectorAll('.btn'); //will give is a NODE LIST, which is array-like. So we can call some methods on it
 
-console.log(buttons);
+buttons.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains('decrease')) {
+      count--;
+    }
+    else if (styles.contains('increase')) {
+      count++;
+    }
+    else {
+      count = 0;
+    }
+    value.textContent = count;
+  });
+});
